eada4c16609412afcf4790daa13715da32c17da9
dsiri12 <dsiri12@gmail.com>
2024-07-26 23:57:43 -0400

    another example edit

---
+++ index4.js
@@ -0,0 +1,6 @@
+// alternate way to have scripts
+function myFunction() {
+
+    // an element's id called demo being gathered and then the text called "Paragraph changed." being changed/added to the element's id called demo
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
