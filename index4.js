8b539e09394e4ba0a1094336406e9c00dbdbcd6a
dsiri12 <dsiri12@gmail.com>
2024-07-26 23:59:20 -0400

    update to example in javascript file

--- index4.js
+++ index4.js
@@ -1,4 +1,4 @@
-// alternate way to have scripts
+// a function being an alternate way to have scripts
 function myFunction() {
 
     // an element's id called demo being gathered and then the text called "Paragraph changed." being changed/added to the element's id called demo
